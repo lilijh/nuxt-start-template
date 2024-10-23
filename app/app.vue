a9d69a50952c096af54c1e72839308b26305b941
bril <1204833842@qq.com>
2024-10-23 13:01:33 +0800

    chore: minor tweak

--- app/app.vue
+++ app/app.vue
@@ -2,14 +2,9 @@
 </script>
 
 <template>
-  <UApp>
-    <UButton>
+  <UApp class="flex flex-col">
+    <UButton class="">
       1
     </UButton>
   </UApp>
 </template>
-
-<style>
-@import "tailwindcss";
-@import "@nuxt/ui";
-</style>
