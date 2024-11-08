638781ca50829f37e58a8f895bd8aed1ad4517c6
bril <1204833842@qq.com>
2024-11-08 15:39:40 +0800

    chore(plugins/request.ts): format

--- app/plugins/request.ts
+++ app/plugins/request.ts
@@ -1,15 +1,14 @@
 export default defineNuxtPlugin(() => {
-    const request = $fetch.create({
-      baseURL: '/api',
-      credentials: 'include',
-      timeout: 5000,
-    })
-  
-    // Expose to useNuxtApp().$request
-    return {
-      provide: {
-        request,
-      },
-    }
+  const request = $fetch.create({
+    baseURL: '/api',
+    credentials: 'include',
+    timeout: 5000,
   })
-  
+
+  // Expose to useNuxtApp().$request
+  return {
+    provide: {
+      request,
+    },
+  }
+})
